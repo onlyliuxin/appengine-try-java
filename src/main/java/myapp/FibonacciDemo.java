696f51d05f45333f37a1d1f9d16e2310a69a582f
onlyliuxin <14703250@qq.com>
2018-01-21 21:15:11 +0800

    add datastore

--- src/main/java/myapp/FibonacciDemo.java
+++ src/main/java/myapp/FibonacciDemo.java
@@ -1,10 +1,20 @@
 package myapp;
 
+import java.util.Date;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
 public class FibonacciDemo extends Thread {
+	private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	
+	
 	public void run(){
-		for (int flag = 1; flag <= 100; flag++) {			
+		for (int flag = 1; flag <= 1000; flag++) {			
 			try {
 			    //dataStore(flag);
+				record(flag);
 				System.out.println("#"+flag);			
 				
 				Thread.sleep(500);
@@ -13,4 +23,11 @@ public class FibonacciDemo extends Thread {
 			}
 		}
 	}
+	 	public void record(int fiboIndex) {
+			Entity fiboTime = new Entity("javaFiboTime");
+			fiboTime.setProperty("FiboIndex", fiboIndex);
+			fiboTime.setProperty("Timestamp", new Date());
+			
+			datastore.put(fiboTime);
+		}
 }
